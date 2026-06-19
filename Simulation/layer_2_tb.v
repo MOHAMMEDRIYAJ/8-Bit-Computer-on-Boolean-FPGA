c6bf3c4810d771a2da4cd64f35945e7a989a262c
Mohammed Riyaj <mdriyaj93454@gmail.com>
2026-06-19 12:00:46 +0530

    Add files via upload

---
+++ Simulation/layer_2_tb.v
@@ -0,0 +1,94 @@
+module tb_cpu_top();
+	reg clk,reset,start,load_btn,inp_loaded;
+	reg [2:0] prog_load;
+	reg [3:0] sw_data;
+	reg [7:0] enter_data;
+	wire inp_req;
+	wire [18:0] sig_out;
+	wire [7:0] bin_display;
+	wire [3:0] pc_out;
+	
+	cpu_top dut(clk,reset,start,prog_load,sw_data,enter_data,load_btn,inp_loaded,inp_req,sig_out,bin_display,pc_out);
+	
+	initial clk = 0;
+	always #5 clk = ~clk;
+	
+	task initialize;
+	begin
+		{reset,start,prog_load,sw_data,load_btn,inp_loaded,enter_data} = 0;
+	end
+	endtask 
+	
+	task Reset;
+	begin	
+		@(negedge clk) reset = 1;
+		@(negedge clk) reset = 0;
+	end
+	endtask 
+	
+	task Layer(input [2:0] layer);
+	begin 
+		@(negedge clk) prog_load = layer;
+	end
+	endtask
+	
+	task Manual_Address (input [3:0] addr);
+	begin	
+		@(negedge clk) sw_data = addr;
+	end
+	endtask
+	
+	task Manual(input [7:0] data);
+	begin	
+		@(negedge clk) enter_data = data;
+	end
+	endtask 
+	
+	task Input_data (input [7:0] inp_data);
+	begin	
+		@(negedge clk) enter_data = inp_data;
+	end
+	endtask
+	
+	task Start;
+	begin
+		@(negedge clk) start = 1;
+		@(negedge clk) start = 0;
+	end
+	endtask
+	
+	task Input_load;
+	begin
+		@(negedge clk) inp_loaded = 1;
+		@(negedge clk) inp_loaded = 0;
+	end
+	endtask
+	
+	task Layer_load;
+	begin
+		@(negedge clk) load_btn = 1;
+		@(negedge clk) load_btn = 0;
+	end
+	endtask
+	
+	initial begin 
+		$monitor("Time=%0t | Reset=%b | Layer=%b | Layer_Load=%b | Start=%b | Output=%b | Halt=%b",$time , reset,prog_load,load_btn,start,bin_display,sig_out[14]);
+		initialize;
+		Reset;
+		Layer(3'b001);
+		Layer_load;
+		Start;
+		wait(inp_req)
+		begin 
+		  @(negedge clk) Input_data(8'd7);
+		  @(negedge clk) Input_load;
+		end
+		wait(inp_req)
+		begin 
+		  @(negedge clk) Input_data(8'd5);
+		  @(negedge clk) Input_load;
+		end      
+		#1000;$finish;
+	end
+		
+endmodule
---
+++ Simulation/manual_layer.v
@@ -0,0 +1,87 @@
+module tb_cpu_top();
+	reg clk,reset,start,load_btn,inp_loaded;
+	reg [2:0] prog_load;
+	reg [3:0] sw_data;
+	reg [7:0] enter_data;
+	wire inp_req;
+	wire [18:0] sig_out;
+	wire [7:0] bin_display;
+	wire [3:0] pc_out;
+	
+	cpu_top dut(clk,reset,start,prog_load,sw_data,enter_data,load_btn,inp_loaded,inp_req,sig_out,bin_display,pc_out);
+	
+	initial clk = 0;
+	always #5 clk = ~clk;
+	
+	task initialize;
+	begin
+		{reset,start,prog_load,sw_data,load_btn,inp_loaded,enter_data} = 0;
+	end
+	endtask 
+	
+	task Reset;
+	begin	
+		@(negedge clk) reset = 1;
+		@(negedge clk) reset = 0;
+	end
+	endtask 
+	
+	task Layer(input [2:0] layer);
+	begin 
+		@(negedge clk) prog_load = layer;
+	end
+	endtask
+	
+	task Manual(input [3:0] addr,input [7:0] data);
+	begin	
+		@(negedge clk) sw_data = addr;
+		@(negedge clk) enter_data = data;
+		end
+	endtask 
+	
+	task Input_data (input [7:0] inp_data);
+	begin	
+		@(negedge clk) enter_data = inp_data;
+	end
+	endtask
+	
+	task Start;
+	begin
+		@(negedge clk) start = 1;
+		@(negedge clk) start = 0;
+	end
+	endtask
+	
+	task Input_load;
+	begin
+		@(negedge clk) inp_loaded = 1;
+		@(negedge clk) inp_loaded = 0;
+	end
+	endtask
+	
+	task Layer_load;
+	begin
+		@(negedge clk) load_btn = 1;
+		@(negedge clk) load_btn = 0;
+	end
+	endtask
+	
+	initial begin 
+		$monitor("Time=%0t | Reset=%b | Layer=%b | Layer_Load=%b | Start=%b | Output=%b | Halt=%b",$time , reset,prog_load,load_btn,start,bin_display,sig_out[14]);
+		initialize;
+		Reset;
+		Layer(3'b111);
+		#10;
+		Manual(4'd0,8'b10011111);
+		Manual(4'd1,8'b01110100);
+		Manual(4'd2,8'b11100000);
+		Manual(4'd3,8'b01100000);
+		Manual(4'd4,8'b11110000);
+		Manual(4'd5,8'b00000000);
+		#10;
+		Layer_load;
+		Start;
+		#100;$stop;
+	end
+		
+endmodule
